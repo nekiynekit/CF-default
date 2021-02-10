7af5099a096bad533279ed77e31aa8127c13f54d
Никита Романов <nik-romanof@yandex.ru>
2021-02-10 16:04:21 +0300

    1st commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <ctime>
+#include <deque>
+#include <queue>
+
+#define int long long
+
+using namespace std;
+
+void solve(){
+    cout << "lol" << endl;
+}
+
+signed main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie();
+    cout.tie();
+
+    int n = 1; //cin >> n;
+    while (n--) solve();
+
+    return 0;
+
+}
